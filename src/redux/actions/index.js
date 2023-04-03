0df26df5f731d6a571673ec25005bb6606a467e6
Daniel Badra <danielbadra17@gmail.com>
2023-04-03 15:34:39 -0300

    Estruturando o Redux

---
+++ src/redux/actions/index.js
@@ -0,0 +1,5 @@
+export const saveProfile = (password, email) => ({
+  type: 'SAVE_PROFILE',
+  password,
+  email,
+});
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootreducer';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
