b9aed720ebc76ced5637f8894c313c206eb33d91
Matheus Milanez <mattmila96@gmail.com>
2023-04-04 15:53:33 -0300

    requisitos 57-62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -10,9 +11,33 @@ export default class Profile extends Component {
 
   render() {
     const { pageName, hasSearchIcon } = this.state;
+    const user = JSON.parse(localStorage.getItem('user'));
     return (
       <div>
         <Header pageName={ pageName } hasSearchIcon={ hasSearchIcon } />
+        <div>
+          <h4 data-testid="profile-email">
+            { user.email }
+          </h4>
+          <Link to="/done-recipes">
+            <button data-testid="profile-done-btn">
+              Done Recipes
+            </button>
+          </Link>
+          <Link to="/favorite-recipes">
+            <button data-testid="profile-favorite-btn">
+              Favorite Recipes
+            </button>
+          </Link>
+          <Link to="/">
+            <button
+              data-testid="profile-logout-btn"
+              onClick={ () => localStorage.clear() }
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
         <Footer />
       </div>
     );
