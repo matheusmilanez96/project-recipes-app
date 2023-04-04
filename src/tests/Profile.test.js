1dfd82c13cea406d89a46f54bf37e5170028d9dc
Matheus Milanez <mattmila96@gmail.com>
2023-04-04 16:27:29 -0300

    testes finalizados

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+
+describe('Testa o componente Profile', () => {
+  it('Testa se o profile funciona corretamente', () => {
+    renderWithRouterAndRedux(<App />);
+
+    const { history } = renderWithRouterAndRedux(<App />);
+    expect(history.location.pathname).toBe('/');
+
+    const email = screen.getAllByTestId('email-input');
+    const password = screen.getAllByTestId('password-input');
+    const button = screen.getAllByTestId('login-submit-btn');
+
+    userEvent.type(email[0], 'final@front.com');
+    userEvent.type(password[0], '1234567');
+    expect(button[0]).not.toBeDisabled();
+
+    userEvent.click(button[0]);
+    waitFor(() => expect(screen.getByText('Meals')).toBeVisible());
+
+    const profileBtn = screen.getAllByTestId('profile-top-btn');
+    userEvent.click(profileBtn[0]);
+
+    const emailEl = screen.getByText('final@front.com');
+    expect(emailEl).toBeInTheDocument();
+
+    const logoutBtn = screen.getByTestId('profile-logout-btn');
+    userEvent.click(logoutBtn);
+    expect(history.location.pathname).toBe('/');
+  });
+});
