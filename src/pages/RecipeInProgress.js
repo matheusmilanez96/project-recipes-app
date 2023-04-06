fef22be936c93a2e35674bae6066fbf9c8134fb1
Matheus Milanez <mattmila96@gmail.com>
2023-04-06 15:20:01 -0300

    requisito 43

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -6,11 +6,14 @@ export default class RecipeInProgress extends Component {
     super();
 
     this.state = {
+      recipe: {},
       thumbnail: '',
       title: '',
       category: '',
       instructions: '',
     };
+
+    this.finishClick = this.finishClick.bind(this);
   }
 
   async componentDidMount() {
@@ -22,28 +25,42 @@ export default class RecipeInProgress extends Component {
       const { meals } = data;
       const recipe = meals[0];
       this.setState({
+        recipe,
         thumbnail: recipe.strMealThumb,
         title: recipe.strMeal,
         category: recipe.strCategory,
         instructions: recipe.strInstructions,
       });
-      console.log(recipe);
     } else {
       const data = await (await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)).json();
       const { drinks } = data;
       const recipe = drinks[0];
       this.setState({
+        recipe,
         thumbnail: recipe.strDrinkThumb,
         title: recipe.strDrink,
         category: recipe.strCategory,
         instructions: recipe.strInstructions,
       });
-      console.log(recipe);
     }
   }
 
+  finishClick(recipe) {
+    const { history } = this.props;
+    if (localStorage.getItem('doneRecipes') !== null) {
+      const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
+      console.log(doneRecipes);
+      const recipeArr = [...doneRecipes, recipe];
+      console.log(recipeArr);
+      localStorage.setItem('doneRecipes', JSON.stringify(recipeArr));
+    } else {
+      localStorage.setItem('doneRecipes', JSON.stringify([recipe]));
+    }
+    history.push('/done-recipes');
+  }
+
   render() {
-    const { thumbnail, title, category, instructions } = this.state;
+    const { recipe, thumbnail, title, category, instructions } = this.state;
     return (
       <div>
         <img
@@ -57,7 +74,13 @@ export default class RecipeInProgress extends Component {
         <button type="button" data-testid="favorite-btn">Favoritar</button>
         <h4 data-testid="recipe-category">{category}</h4>
         <p data-testid="instructions" className="instructionsText">{instructions}</p>
-        <button type="button" data-testid="finish-recipe-btn">Finalizar</button>
+        <button
+          type="button"
+          data-testid="finish-recipe-btn"
+          onClick={ () => this.finishClick(recipe) }
+        >
+          Finalizar
+        </button>
       </div>
     );
   }
