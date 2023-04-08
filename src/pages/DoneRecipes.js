6c7304632d12a7d5c52f2d196649b7c79107b20a
Daniel Badra <danielbadra17@gmail.com>
2023-04-08 17:03:55 -0300

    Requisitos 44 (concluído) 45 (em progresso)

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -7,40 +7,56 @@ export default class DoneRecipes extends Component {
     hasSearchIcon: false,
   };
 
-  componentDidMount() { this.getDoneRecepies(); }
+  componentDidMount() { console.log(this.getDoneRecepies()); }
 
   getDoneRecepies = () => JSON.parse(localStorage.getItem('doneRecipes'));
 
   render() {
     const { pageName, hasSearchIcon } = this.state;
-    const allDoneRecipes = this.getDoneRecepies();
+
     return (
       <div>
         <Header pageName={ pageName } hasSearchIcon={ hasSearchIcon } />
         <button data-testid="filter-by-all-btn">All</button>
         <button data-testid="filter-by-meal-btn">Meals</button>
         <button data-testid="filter-by-drink-btn">Drinks</button>
-        { allDoneRecipes.map((doneRecipe, index) => (
-          <div className="recipeCard" key={ index }>
+        { this.getDoneRecepies().map((doneRecipe, index) => (
+          <div
+            className="recipeCard"
+            key={ doneRecipe.id }
+            style={ { border: '1px solid black',
+              borderRadius: '30px',
+              padding: '10px',
+              margin: '10px',
+              backgroundColor: 'green',
+              color: 'white' } }
+          >
             <img
               data-testid={ `${index}-horizontal-image` }
-              src={ doneRecipe.strMealThumb }
+              src={ doneRecipe.image }
               alt="recipe"
+              style={ { width: '200px', height: '200px', borderRadius: '30px' } }
             />
-            <p data-testid={ `${index}-horizontal-top-text` }>{doneRecipe.strCategory}</p>
-            <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.strMeal}</p>
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              {doneRecipe.nationality}
+              {' '}
+              -
+              {' '}
+              {doneRecipe.category}
+            </p>
+            <p data-testid={ `${index}-horizontal-name` }>{doneRecipe.name}</p>
             <p data-testid={ `${index}-horizontal-done-date` }>
-              {doneRecipe.dateModified}
+              {doneRecipe.doneDate}
             </p>
             <button
               data-testid={ `${index}-horizontal-share-btn` }
             >
               Compartilhar a receita
             </button>
-            {(doneRecipe.strTags === null)
-              ? <div data-testid={ `${index}-${doneRecipe.strTags}-horizontal-tag` } />
+            {(doneRecipe.tags.length === 0)
+              ? <div data-testid={ `${index}-${doneRecipe.tags}-horizontal-tag` } />
               : (
-                <div data-testid={ `${index}-${doneRecipe.strTags}-horizontal-tag` }>
+                <div data-testid={ `${index}-${doneRecipe.tags}-horizontal-tag` }>
                   {doneRecipe.strTags.map((tag) => <p key={ tag }>{tag}</p>)}
                 </div>
               )}
