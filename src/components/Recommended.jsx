7a5b9885539dc63bcd1547166a50bdde09462fc8
ew-fabio <juba.sk8@gmail.com>
2023-04-11 16:07:18 -0300

    requisito 27 - ok

--- src/components/Recommended.jsx
+++ src/components/Recommended.jsx
@@ -3,33 +3,35 @@ import React, { Component } from 'react';
 
 export default class Recommended extends Component {
   state = {
-    counter: 0,
+    card: 0,
   };
 
   handlePrevBtn = () => {
-    const { counter } = this.state;
-    if (counter !== 0) {
+    const { card } = this.state;
+    if (card !== 0) {
       this.setState({
-        counter: counter - 2,
+        card: card - 2,
       });
     }
   };
 
   handleNextBtn = () => {
-    const { counter } = this.state;
+    const { card } = this.state;
     const four = 4;
-    if (counter !== four) {
+    if (card !== four) {
       this.setState({
-        counter: counter + 2,
+        card: card + 2,
       });
     }
   };
 
   render() {
+    const { card } = this.state;
+
     const { recommendedList } = this.props;
     const limit = 6;
     const list = recommendedList.slice(0, limit);
-    const { counter } = this.state;
+
     return (
       <div>
         <h4>Recommended</h4>
@@ -50,30 +52,26 @@ export default class Recommended extends Component {
           >
             Prev
           </button>
-          <div
-            data-testid={ `${list[counter]}-recommendation-card` }
-          >
-            <img
-              src={ list[counter].strMealThumb || list[counter].strDrinkThumb }
-              alt="recommended"
-              width="300px"
-            />
-            <p data-testid={ `${list[counter]}-recommendation-title` }>
-              { list[counter].strMeal || list[counter].strDrink }
-            </p>
-          </div>
-          <div
-            data-testid={ `${list[counter + 1]}-recommendation-card` }
-          >
-            <img
-              src={ list[counter + 1].strMealThumb || list[counter + 1].strDrinkThumb }
-              alt="recommended"
-              width="300px"
-            />
-            <p data-testid={ `${list[counter + 1]}-recommendation-title` }>
-              { list[counter + 1].strMeal || list[counter + 1].strDrink }
-            </p>
-          </div>
+
+          {
+            list.map((item, index) => (
+              <div
+                data-testid={ `${index}-recommendation-card` }
+                key={ index }
+                className={ index === card || index === card + 1 ? 'active' : 'hidden' }
+              >
+                <img
+                  src={ item.strMealThumb || item.strDrinkThumb }
+                  alt="recommended"
+                  width="150px"
+                />
+                <p data-testid={ `${index}-recommendation-title` }>
+                  { item.strMeal || item.strDrink }
+                </p>
+              </div>
+            ))
+          }
+
           <button
             style={ { width: '100px',
               alignSelf: 'center',
@@ -94,17 +92,3 @@ Recommended.propTypes = {
     PropTypes.shape({}),
   ),
 }.isRequired;
-
-// <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-//   <div class="carousel-inner">
-// {list.map((item, index) => (
-//     <div
-// class="carousel-item active"
-// data-testid={ `${index}-recommendation-card` }
-// key={ index }
-// >
-//       <img class="d-block w-100" src={ item.strMealThumb || item.strDrinkThumb } alt="slide">
-//     </div>)
-// }
-//   </div>
-// </div>
