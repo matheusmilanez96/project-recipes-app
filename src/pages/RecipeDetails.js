436fbbe2dde7e8dbafaaa89c02956f9ab5815891
Matheus Milanez <mattmila96@gmail.com>
2023-04-12 15:54:38 -0300

    requisito 34

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -12,9 +12,12 @@ export default class RecipeDetails extends Component {
     super();
 
     this.startClick = this.startClick.bind(this);
+    this.favoriteClick = this.favoriteClick.bind(this);
   }
 
   state = {
+    recipe: {},
+    type: '',
     thumbnail: '',
     title: '',
     category: '',
@@ -34,6 +37,8 @@ export default class RecipeDetails extends Component {
       const { meals } = await (await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)).json();
       const recipe = meals[0];
       this.setState({
+        recipe,
+        type,
         thumbnail: recipe.strMealThumb,
         title: recipe.strMeal,
         category: recipe.strCategory,
@@ -48,6 +53,8 @@ export default class RecipeDetails extends Component {
       const { drinks } = await (await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)).json();
       const recipe = drinks[0];
       this.setState({
+        recipe,
+        type,
         thumbnail: recipe.strDrinkThumb,
         title: recipe.strDrink,
         category: recipe.strCategory,
@@ -60,6 +67,32 @@ export default class RecipeDetails extends Component {
     }
   }
 
+  getMealObj(recipe) {
+    const finalRecipe = {
+      id: recipe.idMeal ? recipe.idMeal : '',
+      type: 'meal',
+      nationality: recipe.strArea ? recipe.strArea : '',
+      category: recipe.strCategory ? recipe.strCategory : '',
+      alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+      name: recipe.strMeal ? recipe.strMeal : '',
+      image: recipe.strMealThumb ? recipe.strMealThumb : '',
+    };
+    return finalRecipe;
+  }
+
+  getDrinkObj(recipe) {
+    const finalRecipe = {
+      id: recipe.idDrink ? recipe.idDrink : '',
+      type: 'drink',
+      nationality: recipe.strArea ? recipe.strArea : '',
+      category: recipe.strCategory ? recipe.strCategory : '',
+      alcoholicOrNot: recipe.strAlcoholic ? recipe.strAlcoholic : '',
+      name: recipe.strDrink ? recipe.strDrink : '',
+      image: recipe.strDrinkThumb ? recipe.strDrinkThumb : '',
+    };
+    return finalRecipe;
+  }
+
   copyToClipboard = () => {
     const { history: { location: { pathname } } } = this.props;
     navigator.clipboard.writeText(`http://localhost:3000${pathname}`);
@@ -67,6 +100,25 @@ export default class RecipeDetails extends Component {
       linkCopied: true,
     });
   };
+
+  favoriteClick(recipe) {
+    const { type } = this.state;
+    let finalRecipe;
+    if (type === 'meals') {
+      finalRecipe = this.getMealObj(recipe);
+    } else {
+      finalRecipe = this.getDrinkObj(recipe);
+    }
+    if (localStorage.getItem('favoriteRecipes') !== null) {
+      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+      const recipeArr = [...favoriteRecipes, finalRecipe];
+      localStorage.setItem('favoriteRecipes', JSON.stringify(recipeArr));
+      console.log(recipeArr);
+    } else {
+      localStorage.setItem('favoriteRecipes', JSON.stringify([finalRecipe]));
+      console.log(finalRecipe);
+    }
+  }
 
   startClick() {
     const { history: { location: { pathname } } } = this.props;
@@ -78,6 +130,7 @@ export default class RecipeDetails extends Component {
 
   render() {
     const {
+      recipe,
       thumbnail,
       title,
       category,
@@ -122,6 +175,7 @@ export default class RecipeDetails extends Component {
           <button
             type="button"
             data-testid="favorite-btn"
+            onClick={ () => this.favoriteClick(recipe) }
           >
             <img src={ favoriteIcon } alt="heart" />
           </button>
